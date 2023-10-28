14c106bcb13e3db8dc93a1eb8d4778fd9e25ca06
Hana Turcke <hanaturcke@leahurstcollege.ca>
2023-10-28 14:51:30 -0400

    change to cpp main file, adds folder to store drivers, updates requirements, clange linter

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hanat on 2023-10-28.
+//
+
+int main() { return 0; };
--- main.py
+++
@@ -1,8 +0,0 @@
-# Author: Hana Turcke
-
-def main():
-    print("hello world")
-
-
-if __name__ == '__main__':
-    main()
