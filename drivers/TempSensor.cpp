a510ec0f851999fd227f68e1346ab485d55c5551
Hana Turcke <hanaturcke@leahurstcollege.ca>
2023-10-31 11:30:44 -0400

    Improve read/writeTemp, fix header/cpp files

--- drivers/TempSensor.cpp
+++ drivers/TempSensor.cpp
@@ -1,102 +1,85 @@
-//
-// Created by hanat on 2023-10-28.
-//
-
 #include "TempSensor.h"
 #include <string>
-#include <iostream>
 #include <map>
-#include <string>
 
 #define MCP9808_I2C_ADDRESS 0x18
 #define MCP9808_REG_AMBIENT_TEMP 0x05
 
 using namespace std;
 
-class TempSensor{
-  // Functionality:
-  //  -get ambient temperature
-  //  -set/get critical temperature
-  //  -set/get upper temperature
-  //  -set/get lower temperature
+TempSensor::TempSensor() {}
 
-private:
-  bool i2c_status;
-  int critical_value;
-  int upper_value;
-  int lower_value;
+TempSensor::TempSensor(bool i2c_status, int critical_value, int upper_value, int lower_value)
+: i2c_status(i2c_status), critical_value(critical_value), upper_value(upper_value), lower_value(lower_value) {
 
-  enum Registers {
-    SLAVE = 0x18,
-    T_UPPER = 0x02,
-    T_LOWER = 0x04,
-    T_CRIT = 0x04,
-    T_AMBIENT = 0x05,
-    MANUFACTURE_ID = 0x06,
-    DEVICE_ID = 0x07,
-    RESOLUTION = 0x08
-  };
+}
 
-public:
-  // constructors
-  TempSensor();
+void TempSensor::reset(){
+  // reset the temperature sensor's critical upper and lower temperatures to 0
+  this->critical_value = 0;
+  this->upper_value = 0;
+  this->lower_value = 0;
+}
 
+/**
+  * Converts a floating point temperature into upper and lower bytes representing an 11 bit number
+  *
+  * @tparam float The celsius floating point temperature
+  *
+  * @return pair<uint8_t, uint8_t> of an upper and lower byte representing 11-bit data in two’s complement format (0.25°C)
+ */
+pair<uint8_t, uint8_t> TempSensor::floatToBytes(float value) {
+  int value_int = static_cast<int>(value * 16);
+  int upper_byte = value_int >> 8;
 
-  TempSensor(bool i2c_status, int critical_value, int upper_value, int lower_value)
-  : i2c_status(i2c_status), critical_value(critical_value), upper_value(upper_value), lower_value(lower_value){
+  // if negative, needs to handle the signed bit
+  if (value_int < 0) {
+    upper_byte |= (1 << 2);
+  }
+  upper_byte &= 0x7;
+  int lower_byte = value_int & 0xFF;
+  return make_pair(static_cast<uint8_t>(upper_byte), static_cast<uint8_t>(lower_byte));
+}
+
+/**
+ * Converts an upper and lower byte representing an 11 bit number into a floating point temperature
+ *
+ * @tparam pair<uint8_t, uint8_t> Pair of an upper and lower byte representing 11-bit data in two’s complement format (0.25°C)
+ *
+ * @return float Returns a celsius floating point temperature
+ */
+float TempSensor::bytesToFloat(pair<uint8_t, uint8_t> bytes) {
+  uint16_t upper_byte = bytes.first;
+  uint16_t lower_byte = bytes.second;
+
+  if (upper_byte & (1 << 2)) {
+    upper_byte = upper_byte | 0b11111100;
+    lower_byte = lower_byte;
+    return (int16_t((upper_byte << 8) | lower_byte)) / 16.0;
   }
 
-  // purpose??
-  void reset(){
-    // reset the temperature sensor's critical upper and lower temperatures to 0
-    this->critical_value = 0;
-    this->upper_value = 0;
-    this->lower_value = 0;
-  }
+  return (int16_t((upper_byte << 8) | lower_byte)) / 16.0;
+}
 
+void TempSensor::writeTemperature(int reg, float value){
+  // Takes temperature value and writes it to the specified register as byte values
+  pair<uint8_t, uint8_t> bytes = floatToBytes(value);
+  uint8_t upperByte = bytes.first;
+  uint8_t lowerByte = bytes.second;
 
-  void writeTemperature(int reg, float value){
-    // writes temperature value to a register for display
+  // depending on the function we use to write to the register
+  // return value of floatToBytes may need to change
 
-    int upperByte, lowerByte;
+  // i2c_bus.write_i2c_block_data(this->registers["slave"], reg, byteArray);
 
-    if(value < 0){
-      int val = int(value * -16);
-      upperByte = val >> 8;   // isolate upper bits
-      lowerByte = val & 0xFF; // isolate lower bits
+}
 
-      upperByte ^= 0x0F;
-      lowerByte ^= 0xFF;
+float TempSensor::readTemperature(int reg){
+  // gets the temperature value from the sensor and returns it as a float
 
-      upperByte |= 0x10; // add sign bit
-    }else {
-      int val = int(value * 16);
-      upperByte = val >> 8;   // isolate upper bits
-      lowerByte = val & 0xFF; // isolate lower bits
-    }
+  pair<uint8_t, uint8_t> tempData(0x1, 0x2); // dummy values
+  // int tempData[2] = i2c_bus.read_i2c_block_data(this->registers["slave"], reg, 2);
 
-    int byteArray[2] = {upperByte, lowerByte};
-
-    // i2c_bus.write_i2c_block_data(this->registers["slave"], reg, byteArray);
-
-  }
-
-  float readTemperature(int reg){
-    int tempData[2] = {0x1, 0x2}; // dummy variable
-    // int tempData[2] = i2c_bus.read_i2c_block_data(this->registers["slave"], reg, 2);
-
-    int upperByte = tempData[0] & 0x1F;
-    int lowerByte = tempData[1] / 16;
-
-    if(upperByte & 0x10){ // if the 5th bit of the upper byte is a 1
-      upperByte = (upperByte & 0xF) * 16;       // multiply by 16 to convert into degrees
-      return -255.75 + (upperByte + lowerByte); // returns temp in celsius
-    }else{
-      upperByte = upperByte * 16;   // multiply
-      return upperByte + lowerByte; // concatenating
-    }
-
-  }
-
-
-};
+  float tempValue = bytesToFloat(tempData);
+  return tempValue;
+}
--- drivers/TempSensor.h
+++ drivers/TempSensor.h
@@ -1,10 +1,46 @@
-//
-// Created by hanat on 2023-10-28.
-//
-
 #ifndef OBC_CURRENT_TEMP_SENSOR_H
 #define OBC_CURRENT_TEMP_SENSOR_H
 
-class temp_sensor {};
+#include <string>
+#include <iostream>
+#include <map>
+#include <string>
+#include <cassert>
+#include <bitset>
+
+using namespace std;
+
+class TempSensor{
+public:
+  // Constructor
+  TempSensor();
+  TempSensor(bool i2c_status, int critical_value, int upper_value, int lower_value);
+
+  // Member functions
+  void reset();
+  pair<uint8_t, uint8_t> floatToBytes(float value);
+  float bytesToFloat(pair<uint8_t, uint8_t> bytes);
+  void writeTemperature(int reg, float value);
+  float readTemperature(int reg);
+
+private:
+  // Attributes
+  bool i2c_status;
+  int critical_value;
+  int upper_value;
+  int lower_value;
+
+  enum Registers {
+    SLAVE = 0x18,
+    T_UPPER = 0x02,
+    T_LOWER = 0x04,
+    T_CRIT = 0x04,
+    T_AMBIENT = 0x05,
+    MANUFACTURE_ID = 0x06,
+    DEVICE_ID = 0x07,
+    RESOLUTION = 0x08
+  };
+};
+
 
 #endif // OBC_CURRENT_TEMP_SENSOR_H
