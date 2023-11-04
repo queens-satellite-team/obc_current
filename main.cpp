00a2088acee3eaa5c3f6227cb32c6a76e458e8bb
c-ola <nikola.z37@hotmail.com>
2023-11-04 15:36:12 -0400

    Fixed cmake working with subdirectories

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 //
 // Created by hanat on 2023-10-28.
 //
-#include "rtc.h"
+#include "drivers/rtc.h"
 
 int main() { 
-    RTC rtc = RTC();
+    RTC* rtc = new RTC(0, 1, 0, "0-1-1 0:0:0");
+    while(true){
+        printf("%d\n", rtc->getSeconds());
+    }
     return 0;
 };
