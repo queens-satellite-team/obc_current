838b8761d5053e6282b072f483e24657e2cff2de
Owen Li <li.owen43@gmail.com>
2023-11-11 18:41:04 -0500

    Added header files to interfaceEPS

---
+++ interfaceEPS.h
@@ -0,0 +1,16 @@
+#define interfaceEPS
+
+extern "C"
+{
+
+}
+
+//Stnadered liberies for I/O files to operate
+#include <iostream>
+#include <unistd.h>
+#include <cmath>
+#include <fcntl.h>
+#include <limits>
+#include <bitset>
+
+//Define I2C adresss for 
