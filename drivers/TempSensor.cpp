0d989410d67f28da55c65755e666db1781cec67a
jaxhodg <jax.h@outlook.com>
2023-11-13 11:31:24 -0500

    Updated TempSensor and added first unittests - Added read16/write16 methods - Commented out main code for testing - Added fff.h to mock functions - Wrote tests for constructor, destructor, and readTemp - Added stubs for write16 & shutdown

--- drivers/TempSensor.cpp
+++ drivers/TempSensor.cpp
@@ -31,12 +31,24 @@ TempSensor::~TempSensor()
   }
 }
 
+uint16_t TempSensor::read16(int reg)
+{
+  uint16_t t = i2c_smbus_read_word_data(i2cFile, reg);
+  t = (t >> 8) | (t << 8);
+  return t;
+}
+
+bool TempSensor::write16(int reg, uint16_t t)
+{
+  t = (t >> 8) | (t << 8);
+  i2c_smbus_write_word_data(i2cFile, reg, t);
+  return true;
+}
+
 float TempSensor::readTemp(int reg)
 {
   float temp = std::numeric_limits<float>::quiet_NaN();
-  uint16_t t = i2c_smbus_read_word_data(i2cFile, reg);
-
-  t = (t >> 8) | (t << 8);
+  uint16_t t = read16(reg);
 
   // std::bitset<16> binary(t);
   // std::cout << binary << std::endl;
@@ -85,7 +97,7 @@ bool TempSensor::writeTemp(int reg, float temp)
 bool TempSensor::shutdown(bool sw)
 {
   uint16_t conf_shutdown;
-  uint16_t conf_register = i2c_smbus_read_word_data(i2cFile, MCP9808_REG_CONFIG);
+  uint16_t conf_register = read16(MCP9808_REG_CONFIG);
 
   conf_register = (conf_register >> 8) | (conf_register << 8);
 
@@ -93,27 +105,27 @@ bool TempSensor::shutdown(bool sw)
   {
     conf_shutdown = conf_register | MCP9808_REG_CONFIG_SHUTDOWN;
     conf_shutdown = (conf_shutdown >> 8) | (conf_shutdown << 8);
-    i2c_smbus_write_word_data(i2cFile, MCP9808_REG_CONFIG, conf_shutdown);
+    write16(MCP9808_REG_CONFIG, conf_shutdown);
   }
   if (sw == false)
   {
     conf_shutdown = conf_register & ~MCP9808_REG_CONFIG_SHUTDOWN;
     conf_shutdown = (conf_shutdown >> 8) | (conf_shutdown << 8);
-    i2c_smbus_write_word_data(i2cFile, MCP9808_REG_CONFIG, conf_shutdown);
+    write16(MCP9808_REG_CONFIG, conf_shutdown);
   }
   return true;
 }
 
-int main()
-{
-  std::cout << "Starting code" << std::endl;
-  TempSensor sensor(1);
+// int main()
+// {
+//   std::cout << "Starting code" << std::endl;
+//   TempSensor sensor(1);
 
-  std::cout << "Ran constructor" << std::endl;
+//   std::cout << "Ran constructor" << std::endl;
 
-  for (int i = 0; i < 5; i++)
-  {
-    std::cout << sensor.readTemp(MCP9808_REG_AMBIENT_TEMP) << std::endl;
-  }
-  return 0;
-}
+//   for (int i = 0; i < 5; i++)
+//   {
+//     std::cout << sensor.readTemp(MCP9808_REG_AMBIENT_TEMP) << std::endl;
+//   }
+//   return 0;
+// }
---
+++ tests/Temperature_Sensor/test_TempSensor.cpp
@@ -0,0 +1,130 @@
+#include "../fff.h"
+#include "../../drivers/TempSensor.h"
+#include <cassert>
+
+DEFINE_FFF_GLOBALS;
+
+// Define fake functions
+FAKE_VALUE_FUNC(int, i2c_smbus_read_word_data, int, uint8_t);
+FAKE_VALUE_FUNC(int, i2c_smbus_write_word_data, int, uint8_t, uint16_t);
+FAKE_VALUE_FUNC_VARARG(int, snprintf, char *, size_t, const char *, ...);
+FAKE_VALUE_FUNC_VARARG(int, open, const char *, int, ...);
+FAKE_VALUE_FUNC_VARARG(int, ioctl, int, unsigned long int, ...);
+FAKE_VALUE_FUNC(int, close, int);
+
+void test_TempSensorConstructor_1()
+{
+    RESET_FAKE(snprintf);
+    RESET_FAKE(open);
+    RESET_FAKE(ioctl);
+    RESET_FAKE(close);
+
+    snprintf_fake.return_val = 1;
+    open_fake.return_val = 2;
+    ioctl_fake.return_val = 3;
+    close_fake.return_val = 4;
+
+    TempSensor sensor(1);
+
+    assert(snprintf_fake.call_count == 1);
+    // Skip rest of snprintf, params are pointers and fixed values
+    // assert(snprintf_fake.arg0_val == ???)
+
+    assert(open_fake.call_count == 1);
+    // filename never gets written to since snprintf is overridden
+    // assert(open_fake.arg0_val == "/dev/i2c-1");
+    assert(open_fake.arg1_val == O_RDWR);
+
+    assert(ioctl_fake.call_count == 1);
+    assert(ioctl_fake.arg0_val == 2);
+    assert(ioctl_fake.arg1_val == I2C_SLAVE);
+    // Variadic parameters are weird, only way to check them is define a whole function?
+    // assert(ioctl_fake.arg2_val == MCP9808_I2CADDR_DEFAULT);
+}
+
+void test_TempSensorDestructor_1()
+{
+    RESET_FAKE(snprintf);
+    RESET_FAKE(open);
+    RESET_FAKE(ioctl);
+    RESET_FAKE(close);
+
+    snprintf_fake.return_val = 1;
+    open_fake.return_val = 2;
+    ioctl_fake.return_val = 3;
+    close_fake.return_val = 4;
+
+    TempSensor sensor(1);
+    sensor.~TempSensor();
+
+    assert(close_fake.call_count == 1);
+    assert(close_fake.arg0_val == 2);
+}
+
+void test_read16_1()
+{
+    // SetsUp i2c_smbus_read_word_data with no history and fake return
+    RESET_FAKE(i2c_smbus_read_word_data);
+    i2c_smbus_read_word_data_fake.return_val = 0x1234;
+
+    // Constructs Sensor and calls read16
+    TempSensor sensor(1);
+    uint16_t t = sensor.read16(0x01);
+
+    // Asserts value is correct
+    assert(t == 0x3412);
+
+    // Asserts i2c_smbus_read_word_data was called correctly
+    assert(i2c_smbus_read_word_data_fake.call_count == 1);
+    assert(i2c_smbus_read_word_data_fake.arg0_val == 2);
+    assert(i2c_smbus_read_word_data_fake.arg1_val == 0x01);
+}
+
+void test_write16_1()
+{
+    // TODO: Set Up i2c_smbus_write_word_data with no history and fake return of true
+
+    // TODO: Construct sensor and call write16 with any 16 bit value;
+
+    // TODO: Assert i2c_smbus_write_word_data was called correctly (remember first and second byte should be flipped)
+}
+
+void test_readTemp_1()
+{
+    // Resetting the FFF call history
+    RESET_FAKE(i2c_smbus_read_word_data);
+
+    // Setting up the fake function
+    i2c_smbus_read_word_data_fake.return_val = 0x65C1;
+
+    // Creating an instance of TempSensor
+    TempSensor sensor(1);
+
+    // Call the method under test
+    float temperature = sensor.readTemp(MCP9808_REG_AMBIENT_TEMP);
+
+    // Check if the temperature is as expected
+    assert(temperature == 22.3125);
+
+    // Asserts i2c_smbus_read_word_data was called correctly
+    assert(i2c_smbus_read_word_data_fake.call_count == 1);
+    assert(i2c_smbus_read_word_data_fake.arg0_val == 2);
+    assert(i2c_smbus_read_word_data_fake.arg1_val == 0x05);
+}
+
+void test_shutdown_1()
+{
+}
+
+int main()
+{
+    test_TempSensorConstructor_1();
+    test_TempSensorDestructor_1();
+    test_read16_1();
+    test_write16_1();
+    test_readTemp_1();
+    test_shutdown_1();
+
+    // Add more tests as needed
+    return 0;
+}
