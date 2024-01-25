956bc629d59fa690b66c233461a861e79743ebee
Lilihines <147667357+Lilihines@users.noreply.github.com>
2024-01-25 15:11:50 -0500

    fixes naming

--- handle_comms.cpp
+++ handle_comms.cpp
@@ -3,13 +3,17 @@
 #include <cstdint>
 #include <vector>
 
-std::unordered_map<uint8_t, void(*)()> methodMap;
+// This code maps function IDs to fuctions using function pointers and unordered maps
+
+// Maps function_id to function_pointer (location of code)
+std::unordered_map<uint8_t, void(*)()> methodMap; // unordered map named methodMap, maps uint8_t keys to function pointers. The function pointers are of type void(*)(), meaning they point to functions that take no arguments and return void.
 
 uint8_t read8(int reg) {
-    // Dummy method, will actually comms data from i2c
+    // This function is a placeholder for reading data, will actually be comms data from i2c
     return 0x01;
 }
 
+//commands available:
 void mode_switch(){
     // Each command reads in its own data
     int param1 = read8(0);
@@ -22,9 +26,10 @@ void file_transfer(){
     int param2 = read8(1);
     int param3 = read8(2);
 }
-
+ 
 
 void callFunction() {
+    // Function is responsible for looking up a function pointer in methodMap based on a given function ID obtained from read8(0). If it finds a matching function, it calls it. Otherwise, it reports an error.
     // Reads function_id
     uint8_t function_id = read8(0);
     // Searches methodMap
