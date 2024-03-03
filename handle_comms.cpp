3b30b0a682096941b83cceda6f67cde7986db0c7
Hana <18ht12@queensu.ca>
2024-03-02 19:58:30 -0500

    adds modeMap and corresponding functions, reorganizes the files too

--- handle_comms.cpp
+++ handle_comms.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <cstdint>
 #include <vector>
+#include "handle_comms.hpp"
+
 // BEFORE YOU MAKE CHANGES:   git fetch
 //                            git pull
 
@@ -9,51 +11,34 @@
 //                            git commit -m "your message here"
 //                            git push
 
-#include "handle_comms.hpp"
-
-// This code maps function IDs to fuctions using function pointers and unordered maps
-
-// Maps function_id to function_pointer (location of code)
-std::unordered_map<uint8_t, std::pair<int, void(*)(std::vector<int> args)> > Comms::methodMap; // unordered map named methodMap, maps uint8_t keys to function pointers. The function pointers are of type void(*)(), meaning they point to functions that take no arguments and return void.
-
 Comms::Comms(){
+    // set the ID's for the "task" functions
     this->methodMap[0] = std::make_pair(0, &Comms::modeSwitch);
     this->methodMap[1] = std::make_pair(1, &Comms::fileTransfer);
+
+    // set the ID's for the "mode" functions
+    this->modeMap[0] = std::make_pair(0, &Comms::defaultMode);
+    this->modeMap[1] = std::make_pair(1, &Comms::mode1);
 }
+
 Comms::~Comms(){}
 
-// key setters
+// method key setters
 void Comms::setModeSwitchKey(int key){
     this->methodMap[0] = std::make_pair(key, &Comms::modeSwitch);
     return;
 }
+
 void Comms::setFileTransferKey(int key){
     this->methodMap[1] = std::make_pair(key, &Comms::fileTransfer);
     return;
 }
 
 
+// helper functions
 uint8_t Comms::read8(int reg) {
     // This function is a placeholder for reading data, will actually be comms data from i2c
     return 1 + reg;
-}
-
-//commands available:
-void Comms::modeSwitch(std::vector<int> args){
-    //Example usage of this function: methodMap[1] = &modeSwitch; 
-    //                                methodMap[2] = &fileTransfer;
-    //                                callFunction();
-    // Each command reads in its own data
-    int param1 = args[0];
-    int param2 = args[1];
-    std::cout << "Switching Mode with arguments: " << param1 << ", " << param2 << std::endl;
-}
-
-void Comms::fileTransfer(std::vector<int> args){
-    int param1 = args[0];
-    int param2 = args[1];
-    int param3 = args[2];
-    std::cout << "Transfering File with arguments: " << param1 << ", " << param2 << ", " << param3 << std::endl;
 }
 
 void Comms::callFunction() {
@@ -97,4 +82,34 @@ void Comms::callFunction() {
         // If result is error, print to cerr
         std::cerr << "Can't find function ID in map" << std::endl;
     }
+}
+
+
+//commands available:
+void Comms::modeSwitch(std::vector<int> args){
+    //Example usage of this function: methodMap[1] = &modeSwitch; 
+    //                                methodMap[2] = &fileTransfer;
+    //                                callFunction();
+    // Each command reads in its own data
+    int param1 = args[0];
+    int param2 = args[1];
+    std::cout << "Switching Mode with arguments: " << param1 << ", " << param2 << std::endl;
+}
+
+void Comms::fileTransfer(std::vector<int> args){
+    int param1 = args[0];
+    int param2 = args[1];
+    int param3 = args[2];
+    std::cout << "Transfering File with arguments: " << param1 << ", " << param2 << ", " << param3 << std::endl;
+}
+
+
+//modes available:
+void Comms::defaultMode(std::vector<int> args){
+    // sets the satellite into default mode
+}
+
+void Comms::mode1(std::vector<int> args){
+    // example mode
+    // calls functions to communicate to the satellite to get into "mode 1"
 }
--- handle_comms.hpp
+++ handle_comms.hpp
@@ -10,15 +10,34 @@
 class Comms {
 
 public:
+    // Maps ID number to function_pointer (location of code)
+    // note: this definition means all methods MUST take in a parameter of type std::vector<int> called args
     static std::unordered_map<uint8_t, std::pair<int, void(*)(std::vector<int> args)> > methodMap;
+
     Comms();
     ~Comms();
+
+    // setters
     void Comms::setModeSwitchKey(int key);
     void Comms::setFileTransferKey(int key);
+
+    // helper functions
     static uint8_t read8(int reg);
+    static void callFunction();
+
+    // "tasks" available
     static void modeSwitch(std::vector<int> args);
     static void fileTransfer(std::vector<int> args);
-    static void callFunction();
+
+    // "modes" available
+    static void defaultMode(std::vector<int> args);
+    static void mode1(std::vector<int> args);
+
+private:
+    // Maps an ID number to a satellite mode function
+    // note: this definition means all modes MUST take in a parameter of type std::vector<int> called args
+    static std::unordered_map<uint8_t, std::pair<int, void(*)(std::vector<int> args)> > modeMap;
+
 };
 
 #endif 
